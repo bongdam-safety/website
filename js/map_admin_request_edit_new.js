89792f072e3733da18d8bca6cc9abbc3ca1bf3be
mgkim1 <mgkim1209@outlook.com>
2024-08-25 01:13:29 +0900

    관리자의 정보수정요청 조회 페이지 생성
    관리자가 사용할 수 있는, 정보수정요청을 확인하고 정보를 수정할 수 있는 페이지를 생성하였습니다.

---
+++ js/map_admin_request_edit_new.js
@@ -0,0 +1,38 @@
+// https://apis.map.kakao.com/web/sample/multipleMarkerImage/
+// admin/request_edit.html 파일의 "지정 위치" 표시를 위해 사용되는 코드입니다.
+
+var newLocationMapContainer = document.getElementById('new_location'), // 지도를 표시할 div  
+    mapOption = { 
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var newMap = new kakao.maps.Map(newLocationMapContainer, mapOption); // 지도를 생성합니다
+ 
+// 마커를 표시할 위치와 title 객체 배열입니다 
+var positions = [
+    {
+        title: '카카오', 
+        latlng: new kakao.maps.LatLng(33.450705, 126.570677)
+    }
+];
+
+// 마커 이미지의 이미지 주소입니다
+var imageSrc = "./../images/marker_cctv.png"; 
+    
+for (var i = 0; i < positions.length; i ++) {
+    
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(24, 53); 
+    
+    // 마커 이미지를 생성합니다    
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+    // 마커를 생성합니다
+    var newMarker = new kakao.maps.Marker({
+        map: newMap, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage // 마커 이미지 
+    });
+}
